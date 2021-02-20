002084ec37053e6b504ef8211229906fa95d3ffe
giriraj <girirajsr10@gmail.com>
2021-02-21 04:48:56 +0530

    Creating 1st API for quiz

--- API/app.js
+++ API/app.js
@@ -2,9 +2,10 @@ const Express = require('express');
 const quizApi = require("./express/quizApi");
 const app = Express();
 
+app.use(Express.json());
 app.use("/quiz", quizApi);
 
-const port = 3000;
+const port = 8090;
 
 
 
